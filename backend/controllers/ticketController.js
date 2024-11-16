b42f01bcd3d8506f22d734875d3f79e44d72500f
Nakul Srivastava <imnakul44@gmail.com>
2024-11-16 08:12:51 +0530

    Get & create Tickets done in backend

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,45 @@ const Ticket = require("../models/ticketModel");
 // @route  GET /api/tickets
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
-   res.status(200).json({ message: "getTcikets" });
+   //get user using the id and jwt
+   const user = await User.findById(req.user.id);
+
+   if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+   }
+
+   const tickets = await Ticket.find({ user: req.user.id });
+
+   res.status(200).json(tickets);
 });
 
 // @desc Create New tIcket
 // @route  POST /api/tickets
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
-   res.status(200).json({ message: "  createTcikets" });
+   const { product, description } = req.body;
+   if (!product || !description) {
+      res.status(400);
+      throw new Error("Please add a product and description");
+   }
+
+   //get user using the id and jwt
+   const user = await User.findById(req.user.id);
+
+   if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+   }
+
+   const ticket = await Ticket.create({
+      product,
+      description,
+      user: req.user.id,
+      status: "new",
+   });
+
+   res.status(200).json(ticket);
 });
 
 module.exports = { getTickets, createTicket };
