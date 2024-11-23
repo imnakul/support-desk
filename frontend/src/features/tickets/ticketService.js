d9074ceb511187c6d7aa39867508f7e4ccf7283d
Nakul Srivastava <imnakul44@gmail.com>
2024-11-23 09:22:44 +0530

    SingleTicketDisplayFunctionality

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -9,9 +9,7 @@ const createTicket = async (ticketData, token) => {
          Authorization: `Bearer ${token}`,
       },
    };
-
    const response = await axios.post(API_URL, ticketData, config);
-
    return response.data;
 };
 
@@ -22,15 +20,25 @@ const getTickets = async (token) => {
          Authorization: `Bearer ${token}`,
       },
    };
-
    const response = await axios.get(API_URL, config);
+   return response.data;
+};
 
+// Get user ticket
+const getTicket = async (ticketId, token) => {
+   const config = {
+      headers: {
+         Authorization: `Bearer ${token}`,
+      },
+   };
+   const response = await axios.get(API_URL + ticketId, config);
    return response.data;
 };
 
 const ticketService = {
    createTicket,
    getTickets,
+   getTicket,
 };
 
 export default ticketService;
